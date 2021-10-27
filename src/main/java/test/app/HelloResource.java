d8cfdd8f7ab9f03075ec3477708bbad5d62c7bfc
gaganasuresh <gagana61298@gmail.com>
2021-10-27 13:43:50 +0530

    change

---
+++ src/main/java/test/app/HelloResource.java
@@ -0,0 +1,18 @@
+package test.app;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloResource {
+    @GetMapping
+    public String helloworld(){
+        return "hello world";
+    }
+
+
+
+}
